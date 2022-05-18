3ab173b3984aabf291fc1a57a9eeff4963bbacac
Jan witczak <j.witczak@onet.pl>
2022-05-18 11:07:40 +0200

    Added RotRate cashing, and rot freeze at -100C

--- NotSoEasyRefrigerationPatch.cs
+++ NotSoEasyRefrigerationPatch.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Collections.Generic;
 using HarmonyLib;
 using Verse;
 
@@ -17,6 +18,14 @@ namespace NotSoEasyRefrigeration
 	[HarmonyPatch(typeof(Verse.GenTemperature), "RotRateAtTemperature")]
 	class RotRatePatch
 	{
+		static Dictionary<int, float> RotRate = new Dictionary<int, float>();
+		static RotRatePatch()
+		{
+			for (int t = -19; t < 10; t++)
+			{
+				RotRate.Add(t, (float)System.Math.Pow(1.16f, t) / 4.411435f); // 1.16^x curve, normalised to 1 at x=10
+			}
+		}
 		static void Postfix(float temperature, ref float __result)
 		{
 			if (temperature >= 10f)
@@ -24,14 +33,19 @@ namespace NotSoEasyRefrigeration
 				__result = 1f;
 				return;
 			}
-			else if (temperature <= -20f)
+			else if (temperature <= -20f && temperature > -100f)
 			{
 				__result = 0.01f;
 				return;
 			}
+			else if (temperature <= -100f)
+			{
+				__result = 0f;
+				return;
+			}
 			else
 			{
-				__result = (float)System.Math.Pow(1.16f, temperature) / 4.411435f;
+				__result = RotRate[(int)temperature];
 				return;
 			}
 		}
